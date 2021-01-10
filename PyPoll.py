9b5e0d821e171f8c19ae82566cb7cc4839c3a138
unknown <frank.dallas@gmail.com>
2021-01-10 13:35:31 -0600

    add work in progress analysis file and folder structure

---
+++ PyPoll.py
@@ -0,0 +1,16 @@
+#%%
+# Packages
+import datetime as dt # current time: dt.datetime.now()
+import csv
+#%%
+# Open the File
+file_to_load = 'Resources/elections_results.csv'
+with open(file_to_load) as election_data:
+    # Get list of all candidate names
+    # Create votecount variable for each variable
+    # Total votes for each name and store in related votecount
+    # Sum all votes to get total vote count
+    # Divide each candidate's votes by total count to get percentage
+
+
+
